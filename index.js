390e3c6690054a12637bee92359f465aa15881d2
Cory <cameyer182@gmail.com>
2023-04-19 14:50:06 -0400

    added form in driver.js

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
-const base_url = "http://127.0.0.1:3000"
+const base_url = "http://127.0.0.1:3000/"
 // const teamService = new TeamService(base_url)
 const driverService = new DriverService(base_url)
 
 // teamService.getTeams()
 driverService.getDrivers()
+Driver.renderForm()
--- src/drvier.js
+++ src/drvier.js
@@ -1,5 +1,7 @@
 class Driver {
     static all = []
+    static driverContainer = document.querySelector('#drivers-container')
+    static formContainer = document.querySelector('#form-container')
     
     constructor({name, description, skill, team_id}) {
         this.name = name
@@ -18,6 +20,21 @@ class Driver {
             <p>${this.skill}</p>
         </div>
     `
+    }
+
+    static renderForm() {
+        Driver.formContainer.innerHTML += `
+            <form id="new-driver-form">
+                <h3>Create a Driver</h3>
+                    Name: <input type="text" id="name">
+                    <br><br>
+                    Description: <textarea id="input-description"></textarea>
+                    <br><br>
+                    Skill: <input type="text" id="skill">
+                    <br><br>
+                    <input type="submit" id="create">
+            </form>
+        `
     }
     
 }
